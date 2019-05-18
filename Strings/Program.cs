8d5f29364d0152e2475f3249e4593eed11298cf7
unknown <ilief.claudiu@gmail.com>
2019-05-18 12:28:39 +0300

    added a text file

---
+++ Strings/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Strings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string MyString = "My awesome string";
+            for (int i = 0; i < MyString.Length; i++)
+            {
+                Console.Write(MyString[i]);
+            }
+            string MyString1 = "My awesome string";
+            Console.Write(MyString1.Length);
+
+            string s = string.Empty;
+            string b = s;
+            Console.Write($"b:{b}");
+
+            int myInt = 30;
+            string myIntAsString = myInt.ToString();
+            Console.WriteLine($"myIntAsString:{myIntAsString}");
+        }
+    }
+}
